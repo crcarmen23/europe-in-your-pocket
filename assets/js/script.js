8bc32cc76f76841f117dcede2349ecf034888c18
Conor <conor.reed@gmail.com>
2023-10-03 12:51:42 -0500

    added some basic Javascript modular functions

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// Chart.js module
+const chartModule = (() => {
+    // Functions related to Chart.js integration
+  })();
+  
+  // API integration modules
+  const currencyApiModule = (() => {
+    // Functions for currency API integration
+  })();
+  
+  const flightsApiModule = (() => {
+    // Functions for flights API integration
+  })();
+  
+  const hotelsApiModule = (() => {
+    // Functions for hotels API integration
+  })();
+  
+  // UI update modules
+  const currencyUIModule = (() => {
+    // Functions to update the currency UI
+  })();
+  
+  const flightsUIModule = (() => {
+    // Functions to update the flights UI
+  })();
+  
+  const hotelsUIModule = (() => {
+    // Functions to update the hotels UI
+  })();
+  
+  // Initialization
+  const init = () => {
+    // Initialize event listeners and other setup
+  };
+  
+  // Run initialization when the DOM is ready
+  document.addEventListener('DOMContentLoaded', () => {
+    init();
+  });
+  
