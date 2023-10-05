8a64b421be7103acab35d0d2dd2ef6ce1eeb153e
ZD0341 <144489565+ZD0341@users.noreply.github.com>
2023-10-05 10:54:22 -0700

    test2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,98 +1,6 @@
 const apiUrl = "https://v6.exchangerate-api.com/v6";
 const APIkey = "6e2a72528ec1fefdb7cb82e0";
 
-<<<<<<< HEAD
-const apiUrl = 'https://v6.exchangerate-api.com/v6';
-const APIkey = '6e2a72528ec1fefdb7cb82e0';
-
-var searchForm = document.getElementById("search-form")
-
-// // Chart.js module
-// const chartModule = (() => {
-//   // Functions related to Chart.js integration
-// })();
-const cacheName = 'my-data-cache';
-window.onload = async () => {
-  await getCodes();
-};
-
-// Function to fetch data and populate the dropdown
-async function getCodes() {
-  try {
-    // Open the cache
-    const cache = await caches.open(cacheName);
-
-    // Check if the data is in the cache
-    const cachedResponse = await cache.match(`${apiUrl}/${APIkey}/codes`);
-
-    if (cachedResponse) {
-      console.log('Using cached response')
-      // If data is found in the cache, use it
-      const data = await cachedResponse.json();
-      
-      var codes = []
-      dropListInput = document.getElementById('dropListInput')
-      dropListOutput = document.getElementById('dropListOutput')
-
-      codes = data.supported_codes
-      for (let i = 0; i < data.supported_codes.length; i++) {
-        //console.log(codes[i][1])
-        var optionIn = document.createElement("option");
-        optionIn.value = codes[i][0];
-        optionIn.text = codes[i][1];
-        dropListInput.appendChild(optionIn);
-
-        var optionOut = document.createElement("option");
-        optionOut.value = codes[i][0];
-        optionOut.text = codes[i][1];
-        dropListOutput.appendChild(optionOut);
-      }
-    } else {
-      console.log('Data not found in cache. Fetching and caching now...');
-
-      // Fetch the data from the server
-      const response = await fetch(`${apiUrl}/${APIkey}/codes`);
-
-      // Store the fetched data in the cache
-      cache.put(`${apiUrl}/${APIkey}/codes`, response.clone());
-      
-      const data = await response.json();
-
-      // Populate the dropdown with the fetched data
-      var codes = []
-      dropListInput = document.getElementById('dropListInput')
-      dropListOutput = document.getElementById('dropListOutput')
-
-      // Clear existing options
-      dropListInput.innerHTML = '';      
-      dropListOutput.innerHTML = '';
-
-      codes = data.supported_codes
-      for (let i = 0; i < data.supported_codes.length; i++) {
-        //console.log(codes[i][1])
-        var optionIn = document.createElement("option");
-        optionIn.value = codes[i][0];
-        optionIn.text = codes[i][1];
-        dropListInput.appendChild(optionIn);
-
-        var optionOut = document.createElement("option");
-        optionOut.value = codes[i][0];
-        optionOut.text = codes[i][1];
-        dropListOutput.appendChild(optionOut);
-      }
-    }
-  } catch (error) {
-    console.error('Error getting data from cache or server:', error);
-  }
-}
-/*
-async function getCodes() {
-
-  var codes = []
-
-  dropListInput = document.getElementById('dropListInput')
-  dropListOutput = document.getElementById('dropListOutput')
-=======
 var searchForm = document.getElementById("search-form");
 
 window.onload = getCodes;
@@ -100,7 +8,6 @@ var codes = [];
 function getCodes() {
   dropListInput = document.getElementById("dropListInput");
   dropListOutput = document.getElementById("dropListOutput");
->>>>>>> c74e242ef288d83e82a4928878a4d7c62b53a873
 
   fetch(`${apiUrl}/${APIkey}/codes`, {
     mode: "cors",
@@ -109,13 +16,8 @@ function getCodes() {
       "Access-Control-Allow-Origin": "*",
     },
   })
-<<<<<<< HEAD
-    .then(response)
-    .then(data => {
-=======
     .then((response) => response.json())
     .then((data) => {
->>>>>>> c74e242ef288d83e82a4928878a4d7c62b53a873
       //Process the data retrieved from the server
       //console.log(data);
       codes = data.supported_codes;
@@ -136,10 +38,6 @@ function getCodes() {
       console.error("Fetch error:", error);
     });
 }
-<<<<<<< HEAD
-*/
-=======
->>>>>>> c74e242ef288d83e82a4928878a4d7c62b53a873
 
 // const conversionUSDtoEUR = data.conversion_rates.EUR
 // const baseUrl = 'https://v6.exchangeratesapi.io/latest';
@@ -151,19 +49,11 @@ const currencyApiModule = (event) => {
   // const url=`${apiUrl}/latest/${APIkey}/USD`
   // const APIkey = '6e2a72528ec1fefdb7cb82e0';
   // Functions for currency API integration
-<<<<<<< HEAD
-  var currencyIn = document.getElementById('dropListInput').value
-  var currencyOut = document.getElementById('dropListOutput').value
-  var input = document.getElementById("calculator-input").value
-  //console.log(currencyIn)
-  //console.log(currencyOut)
-=======
   var currencyIn = document.getElementById("dropListInput").value;
   var currencyOut = document.getElementById("dropListOutput").value;
   var input = document.getElementById("calculator-input").value;
   console.log(currencyIn);
   console.log(currencyOut);
->>>>>>> c74e242ef288d83e82a4928878a4d7c62b53a873
   fetch(`${apiUrl}/${APIkey}/pair/${currencyIn}/${currencyOut}/${input}`, {
     mode: "cors",
     headers: {
@@ -183,32 +73,7 @@ const currencyApiModule = (event) => {
       console.error("Fetch error:", error);
     });
 };
-<<<<<<< HEAD
-searchForm.addEventListener("submit", currencyApiModule)
-
-
-
-// const flightsApiModule = (() => {
-//   const flightApiUrl = "https://api.aviationstack.com/v1/flights";
-//   const flightKey = "12e5b1703d9f0bd7eb3883bb358f97ae";
-
-//   https://api.aviationstack.com/v1/flights?access_key=12e5b1703d9f0bd7eb3883bb358f97ae
-
-//   fetch("https://api.aviationstack.com/v1/flights?access_key=12e5b1703d9f0bd7eb3883bb358f97ae")
-//     .then(res => {
-//       return res.json();
-//     })
-//     .then(data => {
-//       console.log(data);
-//     })
-//     .catch(error => {
-//       console.error("Fetch error:", error);
-//     });
-//   // Functions for flights API integration
-// })();
-=======
 searchForm.addEventListener("submit", currencyApiModule);
->>>>>>> c74e242ef288d83e82a4928878a4d7c62b53a873
 
 const hotelsApiModule = (() => {
   // Functions for hotels API integration
